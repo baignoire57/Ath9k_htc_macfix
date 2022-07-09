7c6e7ebbfb6cee5bb7ac9925e3373211ba766e66
baignoire57 <57708917+baignoire57@users.noreply.github.com>
2022-07-09 23:10:01 +0200

    UP

---
+++ install.sh
@@ -0,0 +1,18 @@
+  GNU nano 6.2                                                                                                     install.sh                                                                                                               
+#!/bin/bash
+
+
+cp ath9k.ko /lib/modules/5.16.0-kali7-amd64/kernel/drivers/net/wireless/ath/ath9k/
+cp ath9k_common.ko /lib/modules/5.16.0-kali7-amd64/kernel/drivers/net/wireless/ath/ath9k/
+cp ath9k_htc.ko /lib/modules/5.16.0-kali7-amd64/kernel/drivers/net/wireless/ath/ath9k/
+cp ath9k_hw.ko /lib/modules/5.16.0-kali7-amd64/kernel/drivers/net/wireless/ath/ath9k/
+
+
+
+
+
+
+
+
+
+
